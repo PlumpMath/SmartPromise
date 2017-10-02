ad8b44ba03b536ffac6336c89c4a1fd5b333aaab
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-10-02 14:01:11 +0300

    #1 save sqllite local db files

--- Startup.cs
+++ Startup.cs
@@ -19,12 +19,14 @@ namespace Promises
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            HostingEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -34,7 +36,7 @@ namespace Promises
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
+            
             services.AddTransient<IPromiseRepository, EFPromiseRepository>();
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
