3b2cc13623f7eb989d36c0f49e5bba1ab7434488
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-11-02 19:48:52 +0300

    #20 nearly resolved (tehre is some bug)

--- wwwroot/js/notification.js
+++ wwwroot/js/notification.js
@@ -8,16 +8,21 @@
     
     const CONTROLLER_NAME = '/api/Friends/'
     const METHOD_GET_PENDING_FRIENDS = 'GetPendingFriends/'
-        
+    const METHOD_ACCEPT_FRIENDSHIP = 'AcceptFriendship/'
+    const METHOD_REJECT_FRIENDSHIP = 'RejectFriendship/'
+
     let unread_messages_count = 0
     let notifications_count = 0
 
     var NotificationsManager = (function() {
-        const NOTIFICATION_LIST_ID = '#_notifications_list'
+        
         const ACCEPT_FRIEND_BUTTON_PREFIX = 'AF'
         const NOT_ACCEPT_FRIEND_BUTTON_PREFIX = 'NAF'
+        const ITEM_PREFIX = 'IP'
+
         const GINGLE_ID = "#_gingle_id"
         const NOTIFICATIONS_LABEL_ID = '#_noti_label_id'
+        const NOTIFICATION_LIST_ID = '#_notifications_list'
 
         let notifications_count = 0
 
@@ -25,23 +30,60 @@
             return user.id.toString()
         }
 
-        function IncreaseNotifications() {
-            ++notifications_count
+        function Show() {
             let text = "Notifications (" + notifications_count + ")"
             $(GINGLE_ID).attr("data-count", notifications_count)
             $(NOTIFICATIONS_LABEL_ID).html(text)
         }
+
+        function IncreaseNotifications() {
+            ++notifications_count
+            Show()
+        }
+
+        function DecreaseNotifications() {
+            --notifications_count
+            Show()
+        }
+
+        function AcceptFriendship(user) {
+            $.get(CONTROLLER_NAME + METHOD_ACCEPT_FRIENDSHIP + user.id, () => RemoveItem(user))
+        }
+
+        function RejectFriendship(user) {
+            $.get(CONTROLLER_NAME + METHOD_REJECT_FRIENDSHIP + user.id, () => RemoveItem(user))
+        }
+
+        function AddAcceptFriendshipHandler(user) {
+            let id = NotificationsManager.GetAcceptFriendButtonId(user)
+            $(id).click(() => AcceptFriendship(user))
+        }
+
+        function AddRejectFriendshipHandler(user) {
+            let id = NotificationsManager.GetNotAcceptFriendButtonId(user)
+            $(id).click(() => RejectFriendship(user))
+        }
+            
+
+        function RemoveItem(user) {
+            let item_id = NotificationsManager.GetItemId(user)
+            $(item_id).remove()
+            DecreaseNotifications()
+        }
         
         return {
-            GetNotAcceptFriendButtonId: user => '#' + ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user)
+            GetNotAcceptFriendButtonId: user => '#' + NOT_ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user)
             ,
 
-            GetAcceptFriendButtonId: user => '#' + NOT_ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user)
+            GetAcceptFriendButtonId: user => '#' + ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user)
             ,
-            
+
+            GetItemId: user => '#' + ITEM_PREFIX + GetItemId(user)
+            ,
+
             AddItem: user => {
                 let element = `
-                        <li class="notification">
+                        <li id="` + ITEM_PREFIX + GetItemId(user) +`" class="notification">
                             <div class="media">
                                 <div class="media-left">
                                     <div class="media-object">
@@ -54,12 +96,12 @@
 
                                     <p>Would like to accept him as a friend?</p>
 
-                                    <button id="`+ NotificationsManager.GetNotAcceptFriendButtonId(user) + `" class="btn btn-success">
-                                        <span class="glyphicon glyphicon-ok" href="#"></span>
+                                    <button type="button" id="`+ ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user) + `" class="btn btn-success">
+                                        <span class="glyphicon glyphicon-ok"></span>
                                     </button> 
 
-                                    <button id="` + NotificationsManager.GetAcceptFriendButtonId(user) + `" class="btn btn-danger">
-                                        <span class="glyphicon glyphicon-remove" href="#"></span>
+                                    <button type="button" id="` + NOT_ACCEPT_FRIEND_BUTTON_PREFIX + GetItemId(user) + `" class="btn btn-danger">
+                                        <span class="glyphicon glyphicon-remove"></span>
                                     </button>
                                     
                                     <!-- <div class="notification-meta">
@@ -71,6 +113,8 @@
                         </li>`
                 $(NOTIFICATION_LIST_ID).append(element)
                 IncreaseNotifications()
+                AddAcceptFriendshipHandler(user)
+                AddRejectFriendshipHandler(user)
             }
         }
     })()
