31b400ea82ea526e805862a5d8a31d9bfe512108
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-11-06 17:58:51 +0300

    minor changes

--- wwwroot/js/helpers.js
+++ wwwroot/js/helpers.js
@@ -1,15 +1,12 @@
-﻿var HELPERS = (function () {
+﻿console.log("______________helpers.js______________")
+var HELPERS = (function () {
     return {
-        GetAvatarImageUrl: (width, height, quality, userId) => {
-
-            let res = _RAZOR_URL_CABINET_GET_AVATAR
-                        .replace("__width__", width.toString())
-                        .replace("__height__", height.toString())
-                        .replace("__quality__", quality.toString())
-                        .replace("__id__", userId.toString())
-            console.log(res)
-            return res
-        }
+        GetAvatarImageUrl: (width, height, quality, userId) =>
+            _RAZOR_URL_CABINET_GET_AVATAR
+                .replace("__width__", width.toString())
+                .replace("__height__", height.toString())
+                .replace("__quality__", quality.toString())
+                .replace("__id__", userId.toString())
     }
-    }
+}
 )()
