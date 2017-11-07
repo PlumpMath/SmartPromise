2ce018b5f7f4ace88726fa815f45dc04ad205dad
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-11-07 15:43:15 +0300

    #24 can add promises

---
+++ wwwroot/js/profilePage.js
@@ -0,0 +1,113 @@
+﻿(function () {
+    console.log("______________profilePage.js______________")
+    const SUMBIT_PROMISE_ID = '#_create_promise_button'
+    const MODAL_PROMISE_ID = '#_fill_promise_modal'
+    const TITLE_ID = "#_promise_title"
+    const CONTENT_ID = "#_promise_content"
+    
+
+    var PromisesListManager = (function () {
+        const PROMISES_LIST_ID = "#_promises_id"
+        
+        let promises = 0
+        let promises_completed = 0
+
+        function GetComplicityBlock(complicity) {
+            const bright_start = `
+                <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                </button>
+            `
+
+            const grey_start = `
+                <button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                </button>
+            `
+            let res = ""
+            for (let i = ComplicityManager.MIN_COMPLICITY; i <= ComplicityManager.MAX_COMPLICITY; ++i) {
+                res += (i <= complicity)? bright_start : grey_start
+            }
+            return res
+        }
+
+        return {
+            AddItem: promise => {
+                let element = `
+                        <div class="review-block">
+                            <div class="row">
+                            <!--
+                            <div class="col-sm-3">
+                                <img src="http://dummyimage.com/60x60/666/ffffff&text=No+Image" class="img-rounded">
+                                <div class="review-block-name"><a href="#">nktailor</a></div>
+                                <div class="review-block-date">January 29, 2016<br />1 day ago</div>
+                            </div>
+                            -->
+                            <div class="col-sm-12">
+                                <div class="review-block-rate">` + GetComplicityBlock(promise.complicity) + ` </div>
+                                <div class="review-block-title">` + promise.title + `</div>
+                                <div class="review-block-description">` + promise.content +`</div>
+                            </div>
+                        </div>
+                    </div>`
+
+                $(PROMISES_LIST_ID).append(element)
+            }
+        }
+    })()
+
+    var ComplicityManager = (function () {
+        const COMPLICITY_CONTROL_ID = '#_complicity_control_id'
+        const MAX_COMPLICITY = 5
+        const MIN_COMPLICITY = 1
+        
+        function GetComplicityButtonId(complicity) {
+            return "#_complicity_" + complicity + "_id"
+        }
+        
+        function ChangeComplicity(complicity) {
+            for (let i = MIN_COMPLICITY; i <= MAX_COMPLICITY; ++i) {
+                let id = GetComplicityButtonId(i)
+                $(id).removeClass(i <= complicity ? "btn-default btn-grey" : "btn-warning")
+                $(id).addClass(i <= complicity ? "btn-warning" : "btn-default btn-grey")
+            }
+        }
+        
+        return {
+            MIN_COMPLICITY,
+            MAX_COMPLICITY,
+            AddHandlers: () => {
+                for (let i = MIN_COMPLICITY; i <= MAX_COMPLICITY; ++i)
+                    $(GetComplicityButtonId(i)).click(() => ChangeComplicity(i))
+            },
+            GetComplicity: () => {
+                let complicity = 0
+                $(COMPLICITY_CONTROL_ID + ' > button').each(
+                    function () {
+                        if ($(this).hasClass("btn-warning"))
+                            ++complicity
+                    })
+                return complicity
+            }
+        }
+
+    })()
+
+    function GetPromise() {
+        return {
+            title: $(TITLE_ID).val(),
+            content: $(CONTENT_ID).val(),
+            complicity: ComplicityManager.GetComplicity()
+        }
+    }
+
+    window.onload = () => {
+        ComplicityManager.AddHandlers()
+        $(SUMBIT_PROMISE_ID).click(() => {
+
+            let promise = GetPromise()
+            PromisesListManager.AddItem(promise)
+            $(MODAL_PROMISE_ID).modal("hide")
+        })
+    }
+})()
