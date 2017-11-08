b7ce15411427f0cd29f52b307f5369031748ba22
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-11-08 04:25:49 +0300

    resolve #24

--- wwwroot/js/profilePage.js
+++ wwwroot/js/profilePage.js
@@ -8,9 +8,26 @@
     var PromisesListManager = (function () {
         const PROMISES_LIST_ID = "#_promises_id"
         const COMPLETED_RATING_ID = "#_completed_rating_id"
+        const COMPLETE_PROMISE_MODAL_ID = "#_complete_promise"
+        const COMPLETE_PROMISE_BTN_ID = '#_complete_promise_btn'
+        const BUTTON_PREFIX = "BP"
+        const PROMISE_PREFIX = "PP"
+
         let promises = 0
         let promises_completed = 0
 
+        function GetPromiseId(key) {
+            return '#' + PROMISE_PREFIX + GetKey(key)
+        }
+
+        function GetCompleteButtonId(key) {
+            return '#' + BUTTON_PREFIX + GetKey(key)
+        }
+
+        function GetKey(index) {
+            return index.toString()
+        }
+        
         function GetComplicityBlock(complicity) {
             const bright_start = `
                 <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
@@ -31,14 +48,31 @@
         }
 
         function UpdateStatistics() {
-            let rate = completed === 0? 0 : promises / completed
-            $(COMPLETED_RATING_ID).html(rate)
+            console.log(promises_completed + "      " + promises)
+            let rate = promises_completed === 0 ? 0 : ((promises_completed / promises) * 5).toFixed(2)
+            $(COMPLETED_RATING_ID).html(rate + " <small>/ 5</small>")
+        }
+
+        function UpdateAsCompleted(key) {
+            $(GetPromiseId(key)).addClass("promise-block-completed")
+            $(GetCompleteButtonId(key)).hide()
+            ++promises_completed
+            UpdateStatistics()
+        }
+
+        function AddButtonHandler(key) {
+            $(GetCompleteButtonId(key)).click(() => {
+                $(COMPLETE_PROMISE_MODAL_ID).modal("toggle")
+                $(COMPLETE_PROMISE_BTN_ID).unbind('click').click(() => UpdateAsCompleted(key))
+            })
         }
 
         return {
             AddItem: promise => {
+                let promiseKey = PROMISE_PREFIX + GetKey(promises)
+                let buttonKey = BUTTON_PREFIX + GetKey(promises)
                 let element = `
-                        <div class="promise-block">
+                        <div id="` + promiseKey + `"class="promise-block">
                             <div class="row">
                             <!--
                             <div class="col-sm-3">
@@ -54,15 +88,18 @@
                             </div>
                             <div class="col-sm-3">
                                 <span class="pull-right">
-                                    <button type="button" class="btn btn-success">
+                                    <button id="` + buttonKey + `" type="button" class="btn btn-success">
                                         <span class="glyphicon glyphicon-ok"></span>
                                     </button>
                                 </span>
                             </div>
                         </div>
                     </div>`
-
+                
+                var key = GetKey(promises)
+                console.log(key)
                 $(PROMISES_LIST_ID).append(element)
+                AddButtonHandler(key)
                 ++promises
                 UpdateStatistics()
             }
@@ -117,10 +154,9 @@
     window.onload = () => {
         ComplicityManager.AddHandlers()
         $(SUMBIT_PROMISE_ID).click(() => {
-
             let promise = GetPromise()
             PromisesListManager.AddItem(promise)
-            $(MODAL_PROMISE_ID).modal("hide")
+            $(MODAL_PROMISE_ID).modal("toggle")
         })
     }
 }
